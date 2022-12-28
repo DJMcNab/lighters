fce7846f6980bc21c7da5b9c9a89b00ae1fe44a0
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2022-12-28 22:01:27 +0000

    Clarify some thoughts on spans

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 use naga::{Function, Module, Span};
 
+/// We unfortunately need to use a null span for all operations, because naga only understands single-file
+/// sources. In theory, we could use `Span` as a `u64` identifier, although since wgpu would panic if the error was printed
+/// (e.g. with a naïve `unwrap`), that's not ideal
+///
+/// It may be fruitful to store our own spans in a side-channel for our own error reporting if there is a naga validation failures, or
+/// adapt wgpu to handle this case "gracefully"
+///
+/// It seems surprising that the spir-v or glsl backends don't run into this, although I haven't looked into it
+const SPAN: Span = Span::UNDEFINED;
+
 pub fn module() -> Module {
     let mut module = Module::default();
     let function = Function::default();
-    module.functions.append(function, Span::new(0, 10));
+    module.functions.append(function, SPAN);
     return module;
 }
