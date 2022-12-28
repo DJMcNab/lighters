aafd72b9bfa174de76370b4b61daf29644abad63
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2022-12-28 22:48:12 +0000

    Initial support for naming types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use naga::{Function, Module, Span};
+pub mod types;
+
+use naga::{
+    EntryPoint, Expression, Function, FunctionArgument, Module, Span, Statement, Type, TypeInner,
+};
 
 /// We unfortunately need to use a null span for all operations, because naga only understands single-file
 /// sources. In theory, we could use `Span` as a `u64` identifier, although since wgpu would panic if the error was printed
@@ -10,9 +14,59 @@ use naga::{Function, Module, Span};
 /// It seems surprising that the spir-v or glsl backends don't run into this, although I haven't looked into it
 const SPAN: Span = Span::UNDEFINED;
 
+use types::MyStruct;
+pub use types::TypeMap;
+pub use types::{ToType, TypeRegistry};
+
 pub fn module() -> Module {
+    let mut types = TypeMap::default();
     let mut module = Module::default();
-    let function = Function::default();
-    module.functions.append(function, SPAN);
+    let mut registry = TypeRegistry::new(&mut module, &mut types);
+    registry.register_type::<MyStruct>();
+    let ty = module.types.insert(
+        Type {
+            inner: TypeInner::Scalar {
+                kind: naga::ScalarKind::Sint,
+                width: 4,
+            },
+            name: None,
+        },
+        SPAN,
+    );
+    let mut function = Function::default();
+    function.arguments.push(FunctionArgument {
+        name: Some("oh_neat".to_string()),
+        ty,
+        binding: Some(naga::Binding::Location {
+            location: 0,
+            interpolation: None,
+            sampling: None,
+        }),
+    });
+    let arg_expr = function
+        .expressions
+        .append(Expression::FunctionArgument(0), SPAN);
+    // let arg_load_expr = function
+    //     .expressions
+    //     .append(Expression::Load { pointer: arg_expr }, SPAN);
+    let named = function.expressions.append(
+        naga::Expression::Unary {
+            op: naga::UnaryOperator::Negate,
+            expr: arg_expr,
+        },
+        SPAN,
+    );
+    function.named_expressions.insert(named, "val".to_string());
+    function
+        .body
+        .push(Statement::Emit(function.expressions.range_from(0)), SPAN);
+    let entry_point = EntryPoint {
+        name: "main".to_string(),
+        stage: naga::ShaderStage::Compute,
+        early_depth_test: None,
+        workgroup_size: [256, 1, 1],
+        function,
+    };
+    module.entry_points.push(entry_point);
     return module;
 }
---
+++ src/types.rs
@@ -0,0 +1,123 @@
+use naga::{Handle, Module, ScalarKind, StructMember, Type, TypeInner};
+use std::{
+    any::TypeId,
+    collections::HashMap,
+    ops::{Deref, DerefMut},
+};
+
+use crate::SPAN;
+
+pub trait ToType: 'static + Sized {
+    fn naga_ty(registry: &mut TypeRegistry) -> Type {
+        Type {
+            name: Some(type_name_simple_of::<Self>()),
+            inner: Self::naga_ty_inner(registry),
+        }
+    }
+    fn naga_ty_inner(registry: &mut TypeRegistry) -> TypeInner;
+}
+
+fn type_name_simple_of<T: 'static>() -> String {
+    let name = std::any::type_name::<T>();
+    let name_final = name.rsplit_terminator("::").next().unwrap();
+    name_final.replace("<", "_").replace(">", "_")
+}
+
+impl ToType for u32 {
+    fn naga_ty_inner(_: &mut TypeRegistry) -> TypeInner {
+        naga::TypeInner::Scalar {
+            kind: ScalarKind::Uint,
+            width: 4,
+        }
+    }
+}
+impl ToType for i32 {
+    fn naga_ty_inner(_: &mut TypeRegistry) -> TypeInner {
+        naga::TypeInner::Scalar {
+            kind: ScalarKind::Sint,
+            width: 4,
+        }
+    }
+}
+impl ToType for f32 {
+    fn naga_ty_inner(_: &mut TypeRegistry) -> TypeInner {
+        naga::TypeInner::Scalar {
+            kind: ScalarKind::Float,
+            width: 4,
+        }
+    }
+}
+impl ToType for bool {
+    fn naga_ty_inner(_: &mut TypeRegistry) -> TypeInner {
+        naga::TypeInner::Scalar {
+            kind: ScalarKind::Bool,
+            width: naga::BOOL_WIDTH,
+        }
+    }
+}
+
+pub struct MyStruct {
+    pub a: u32,
+    pub b: f32,
+}
+
+impl ToType for MyStruct {
+    fn naga_ty_inner(registry: &mut TypeRegistry) -> TypeInner {
+        let members = vec![
+            StructMember {
+                name: Some("a".to_string()),
+                ty: registry.register_type::<u32>(),
+                binding: None,
+                offset: 0,
+            },
+            StructMember {
+                name: Some("b".to_string()),
+                ty: registry.register_type::<f32>(),
+                binding: None,
+                offset: 4,
+            },
+        ];
+        TypeInner::Struct {
+            members,
+            span: std::mem::size_of::<Self>().try_into().unwrap(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct TypeMap {
+    map: HashMap<TypeId, Handle<Type>>,
+}
+
+impl Deref for TypeMap {
+    type Target = HashMap<TypeId, Handle<Type>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.map
+    }
+}
+
+impl DerefMut for TypeMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.map
+    }
+}
+
+pub struct TypeRegistry<'a> {
+    tys: &'a mut TypeMap,
+    module: &'a mut Module,
+}
+
+impl<'a> TypeRegistry<'a> {
+    pub fn new(module: &'a mut Module, tys: &'a mut TypeMap) -> Self {
+        Self { tys, module }
+    }
+
+    pub fn register_type<T: ToType>(&mut self) -> Handle<Type> {
+        if let Some(ty) = self.tys.get(&TypeId::of::<T>()) {
+            return ty.clone();
+        }
+        let ty_val = T::naga_ty(self);
+        self.module.types.insert(ty_val, SPAN)
+    }
+}
